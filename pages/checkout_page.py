188e868f90e994b2bc976ee4da12df27ea510012
muhamadaqza <muhamad.aqza@gmail.com>
2026-08-24 12:57:32 +0700

    feat: complete full e2e checkout flow with checkout page object

---
+++ pages/checkout_page.py
@@ -0,0 +1,21 @@
+class CheckoutPage:
+    def __init__(self, page):
+        self.page = page
+        self.first_name_input = page.locator("#first-name")
+        self.last_name_input = page.locator("#last-name")
+        self.postal_code_input = page.locator("#postal-code")
+        self.continue_button = page.locator("#continue")
+        self.finish_button = page.locator("#finish")
+        self.complete_header = page.locator(".complete-header")
+
+    def fill_information(self, first_name, last_name, postal_code):
+        self.first_name_input.fill(first_name)
+        self.last_name_input.fill(last_name)
+        self.postal_code_input.fill(postal_code)
+        self.continue_button.click()
+
+    def finish_checkout(self):
+        self.finish_button.click()
+
+    def get_completion_message(self):
+        return self.complete_header.text_content()
--- tests/test_e2e_checkout.py
+++ tests/test_e2e_checkout.py
@@ -1,17 +1,27 @@
 import pytest
 from pages.login_page import LoginPage
 from pages.inventory_page import InventoryPage
+from pages.checkout_page import CheckoutPage
 
-def test_e2e_tambah_produk_ke_keranjang(page):
+def test_e2e_checkout_sampai_selesai(page):
     login_page = LoginPage(page)
     inventory_page = InventoryPage(page)
+    checkout_page = CheckoutPage(page)
 
-    # 1. Buka halaman login & jalankan autentikasi
+    # 1. Login
     login_page.navigate()
     login_page.login("standard_user", "secret_sauce")
 
-    # 2. Tambahkan produk ke keranjang belanja
+    # 2. Tambah produk ke keranjang & masuk ke halaman checkout
     inventory_page.add_backpack_to_cart()
+    inventory_page.cart_link.click()
+    page.locator("#checkout").click()
 
-    # 3. Verifikasi jumlah item pada ikon keranjang
-    assert inventory_page.get_cart_count() == "1"
+    # 3. Isi form data pengiriman
+    checkout_page.fill_information("Muhammad", "Aqza", "40123")
+
+    # 4. Selesaikan transaksi
+    checkout_page.finish_checkout()
+
+    # 5. Verifikasi pesan sukses transaksi
+    assert checkout_page.get_completion_message() == "Thank you for your order!"
